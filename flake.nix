f42495b8acf7e840f35d04d0153b375918828926
Anil Anar <anilanar@hotmail.com>
2021-07-23 19:00:00 +0200

    wip

--- flake.nix
+++ flake.nix
@@ -17,6 +17,7 @@
           home-manager.useGlobalPkgs = true;
           home-manager.useUserPackages = true;
           home-manager.users.aanar = import ./linux.nix;
+          age.secrets.secret.file = ./secrets/secret.age;
         }
       ];
     };
