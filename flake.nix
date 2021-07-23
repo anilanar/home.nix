6f9b63efbbdfcde1800cbdd60f90f08eee6b77c1
Anil Anar <anilanar@hotmail.com>
2021-07-23 18:50:39 +0200

    wip

--- flake.nix
+++ flake.nix
@@ -2,15 +2,17 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-21.05";
     home-manager.url = "github:nix-community/home-manager/release-21.05";
+    agenix.url = "github:ryantm/agenix";
   };
 
-  outputs = { self, nixpkgs, home-manager }: {
+  outputs = { self, nixpkgs, home-manager, agenix }: {
 
     nixosConfigurations.aanar-nixos = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       modules = [
         ./configuration.nix
         home-manager.nixosModules.home-manager
+        agenix.nixosModules.age
         {
           home-manager.useGlobalPkgs = true;
           home-manager.useUserPackages = true;
