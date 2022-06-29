d51d8143433de7c68483202804ed57855ea542e8
Anil Anar <anilanar@hotmail.com>
2022-06-29 12:44:01 +0200

    add tabula ffxi

--- apps/ffxi.nix
+++ apps/ffxi.nix
@@ -2,10 +2,14 @@
 
 let
   wine = pkgs.wineWowPackages.staging;
-  ffxi = pkgs.writeScriptBin "ffxi" ''
+  wings = pkgs.writeScriptBin "wings" ''
     #!${pkgs.stdenv.shell}
     cd /d8a/wine/wingsxi-64/drive_c/WingsXI/Ashita
     WINEARCH=win64 WINEPREFIX=/d8a/wine/wingsxi-64 ${wine}/bin/wine injector.exe nectar.xml
   '';
-in { home.packages = [ ffxi wine pkgs.winetricks unstable.lutris ]; }
-
+  tabula = pkgs.writeScriptBin "tabula" ''
+    #!${pkgs.stdenv.shell}
+    cd /d8a/wine/tabula-rasa/drive_c/TabulaRasa/PlayOnline/Ashita
+    WINEARCH=win64 WINEPREFIX=/d8a/wine/tabula-rasa ${wine}/bin/wine injector.exe pluton.xml
+  '';
+in { home.packages = [ wings tabula wine pkgs.winetricks ]; }
