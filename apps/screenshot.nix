33559d39a340abad4bf06c3b153c80bd756cd776
Anil Anar <anilanar@hotmail.com>
2022-07-10 12:14:37 +0200

    fix screenshot

--- apps/screenshot.nix
+++ apps/screenshot.nix
@@ -3,7 +3,7 @@
 let
   screenshot = pkgs.writeScriptBin "screenshot" ''
     #!${pkgs.stdenv.shell}
-    ${pkgs.scrot}/bin/scrot -sf --line style=dash,width=3,color=red ${config.home.homeDirectory}/Pictures/%Y-%m-%d-%H-%M-%S.png
+    ${pkgs.scrot}/bin/scrot -s -f --line style=dash,width=3,color=red ${config.home.homeDirectory}/Pictures/%Y-%m-%d-%H-%M-%S.png
   '';
 in { home.packages = [ screenshot ]; }
 
