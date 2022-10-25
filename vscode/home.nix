db321b0a0fb64152d15f8996091334e5a59af580
Anil Anar <anilanar@hotmail.com>
2022-10-25 12:23:51 +0200

    try replace vscode with unstable vscodium

--- vscode/home.nix
+++ vscode/home.nix
@@ -1,4 +1,4 @@
-{ pkgs, ... }:
+{ pkgs, unstabe, ... }:
 let
   exts = import ./vscode-extensions.nix { inherit pkgs; };
   config = import ./vscode-config.nix { inherit pkgs; };
@@ -6,6 +6,7 @@ in {
   home.packages = [ (import ./update-vscode-exts.nix { inherit pkgs; }) ];
   programs.vscode = {
     enable = true;
+    package = unstable.vscodium;
     extensions = with exts; [ nix vim nixfmt ];
     userSettings = config.userSettings;
     keybindings = config.keybindings;
